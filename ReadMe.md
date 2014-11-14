84d5d2267f17dd981623f902633da45bbea1bd05
ethanzhu <764724624@qq.com>
2014-11-14 16:40:42 +0800

    重新规划目录结构 更新插件

--- ReadMe.md
+++
@@ -1,6 +0,0 @@
-#四达投资抢标助 (google浏览器插件)
-##使用说明
-> 用WebKit内核的浏览器如chrome、猎豹浏览器等，下载qb.crx安装即可:[插件下载地址][1]
-
-
-[1]: http://git.oschina.net/ethanzhu/open_project/blob/google%E6%8F%92%E4%BB%B6-%E5%9B%9B%E8%BE%BE%E6%8A%95%E8%B5%84%E6%8A%A2%E6%A0%87%E5%8A%A9%E6%89%8B/qb.crx
---
+++ qb/content_script.js
@@ -0,0 +1,78 @@
+﻿// var postInfo = $("div.postDesc");
+// if(postInfo.length!=1){
+// 	chrome.runtime.sendMessage({type:"cnblog-article-information", error:"获取文章信息失败."});
+// }
+// else{
+// 	var msg = {
+// 		type: "cnblog-article-information",
+// 		title : $("#cb_post_title_url").text(),
+// 		postDate : postInfo.find("#post-date").text(),
+// 		author : postInfo.find("a").first().text(),
+// 		url: document.URL
+// 	};
+// 	chrome.runtime.sendMessage(msg);
+// }
+// 
+// 
+// 
+var profiles;
+var interV;
+//接收消息
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {  
+	profiles=request.msg;
+	if(profiles==undefined){
+		alert("请先设置抢标助手");
+	}else if(profiles.isAutoRefash=="yes"){
+       interV=setInterval("unlockTime()",500);
+	}
+    console.log('收到消息' + request.msg);
+}); 
+//var Settings = extension.Settings;
+
+$("#fast").after("<input type='button' id='to_submit' value='一键提交'/>");
+// $("#fast").after("<input type='button' id='to_submit' value='一键提交'/>"
+// +"<script>"
+// 	+"$('#to_submit').click(function(){"
+// +"document.getElementById('fast').click();"
+// +" $('#tpwd').val('05170621');"
+// +" document.getElementById('buybt').click();"
+// +"});"
+// +"</script>");
+// if(profiles.isAutoRefash=="yes")
+//    setInterval("to_submit()",1000);
+// $("head").append("<script>"
+// 	+"$('#to_submit').click(function(){"
+// 	+"alert('okkkk');"
+// +"	  $('#buytxt').val('1');"
+// +" $('#tpwd').val('05170621');"
+// +" document.getElementById('buybt').click();"
+// +"});"
+// +"</script>");
+$("#to_submit").click(function(){
+	  to_submit();
+
+});
+
+
+var i=0;
+function unlockTime(){
+	var unlockTime=$("#times").html();
+	if(unlockTime=="0秒后解锁" && i<=2 || unlockTime=="2秒后解锁" && i==1 ||unlockTime=="3秒后解锁" && i==0){
+
+		if(unlockTime=="0秒后解锁" && i<=2){
+		   clearInterval(interV);
+        }
+		//if(i==0){
+		   to_submit();
+		   i++;
+		//}
+		
+	}
+}
+
+
+function to_submit(){	
+  document.getElementById('fast').click();
+  $("#tpwd").val(profiles!=undefined?profiles.pw:"88888888");
+  document.getElementById('buybt').click();
+}
