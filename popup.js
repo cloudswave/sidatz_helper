310f94b6d342771f6f70bc0cec63d0181e5ed2d2
ethanzhu <764724624@qq.com>
2014-11-14 16:30:19 +0800

    手动点击图标可以刷新招标情况

--- popup.js
+++ popup.js
@@ -16,12 +16,15 @@ $("#page_refresh_stop").click(function(){
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-	var background=chrome.extension.getBackgroundPage()
+	var background=chrome.extension.getBackgroundPage();
+
 	var data = background.articleData;
 	if(background.run_task){
 	  $("#message").text("正在每"+background.mill+"毫秒刷新一次招标信息...");
+
 	}else{
-		$("#message").text("已停止刷新获取招标信息!");
+		$("#message").text("已停止自动刷新获取招标信息!");
+		background.getData();
 	}
 	if(data.error){
 		$("#message").text(data.error);
