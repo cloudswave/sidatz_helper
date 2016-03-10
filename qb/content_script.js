0a882a78bbea0c09791f878b48d1856b7d83a4ab
ethanzhu <764724624@qq.com>
2016-03-10 19:49:42 +0800

    修复 密码错误bug

--- qb/content_script.js
+++ qb/content_script.js
@@ -23,32 +23,21 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	if(profiles==undefined){
 		alert("请先设置抢标助手");
 	}else if(profiles.isAutoRefash=="yes"){
+	   to_submit();
+	   console.log('提交一次，开始循环监听');
        interV=setInterval("unlockTime()",500);
 	}
+	$("#chosehb").click();//打开选红包
+	console.log('打开红包界面');
     console.log('收到消息' + request.msg);
 }); 
 //var Settings = extension.Settings;
 
 $(".backgroundred").after("<input type='button' id='to_submit' value='一键提交'/>");
-$("#chosehb").click();//打开选红包
-// $("#fast").after("<input type='button' id='to_submit' value='一键提交'/>"
-// +"<script>"
-// 	+"$('#to_submit').click(function(){"
-// +"document.getElementById('fast').click();"
-// +" $('#tpwd').val('05170621');"
-// +" document.getElementById('buybt').click();"
-// +"});"
-// +"</script>");
-// if(profiles.isAutoRefash=="yes")
-//    setInterval("to_submit()",1000);
-// $("head").append("<script>"
-// 	+"$('#to_submit').click(function(){"
-// 	+"alert('okkkk');"
-// +"	  $('#buytxt').val('1');"
-// +" $('#tpwd').val('05170621');"
-// +" document.getElementById('buybt').click();"
-// +"});"
-// +"</script>");
+$(".backgroundred").click();
+console.log('backgroundred click');
+
+
 $("#to_submit").click(function(){
 	  to_submit();
 
@@ -81,5 +70,6 @@ function unlockTime(){
 function to_submit(){	
   $(".backgroundred").click();
   $("#TPassword").val(profiles!=undefined?profiles.pw:"88888888");
+  console.log("password:"+profiles.pw);
   $(".BuyTarGet").click();
 }
